95320bf18a8d588f6fd123aa0e76eb8d80ace99f
arnaudlumini <arnaud@lumini.fr>
2015-08-29 17:16:56 +0200

    Hardware tests.

--- src/Pi.class.js
+++ src/Pi.class.js
@@ -2,17 +2,17 @@ var wpi = require('wiring-pi');
 
 function Pi() {}
 
-Pi.RED = 'red';
-Pi.BLUE = 'blue';
+Pi.BLUE_VICTORY_PIN = 2;
+Pi.RED_VICTORY_PIN = 3;
 
 Pi.PRESS_BLUE_PIN = 7;
 Pi.PRESS_RED_PIN = 0;
-Pi.PRESS_START_PIN = 0;
+Pi.PRESS_START_PIN = 1;
 
 Pi.instance = undefined;
 
 Pi.getInstance = function() {
-  if (Pi.instance !== undefined) {
+  if (Pi.instance === undefined) {
     Pi.instance = new Pi();
     Pi.instance.init();
   }
@@ -26,19 +26,23 @@ Pi.prototype.init = function() {
   wpi.pinMode(Pi.PRESS_BLUE_PIN, wpi.OUTPUT); //pin 7
   wpi.pinMode(Pi.PRESS_RED_PIN, wpi.OUTPUT); //pin 11
   wpi.pinMode(Pi.PRESS_START_PIN, wpi.OUTPUT); //pin 12
-	
-  //wpi.pinMode(2, wpi.INPUT); //pin 13
-  //wpi.pinMode(3, wpi.INPUT); //pin 15
+  
+  //wpi.pinMode(, wpi.INPUT); //pin 13
+  wpi.pinMode(Pi.RED_VICTORY_PIN, wpi.INPUT); //pin 15
 };
 
 Pi.prototype.press = function(pin) {
-  wpi.digitalWrite(pin, wpi.HIGH);
+  wpi.digitalWrite(pin, wpi.LOW);
   setTimeout(function() {
-  	  wpi.digitalWrite(pin, wpi.LOW);
-  }, 50);
+  	  wpi.digitalWrite(pin, wpi.HIGH);
+  }, 500);
 };
 
-Pi.prototype.watch = function() {
+Pi.prototype.watch = function(pin) {
+  setInterval(function() {
+	var value = wpi.digitalRead(pin);
+	console.info(value);
+  }, 500);
 };
 
 module.exports = Pi;
---
+++ test.js
@@ -0,0 +1,3 @@
+var Pi = require('./src/Pi.class.js');
+//Pi.getInstance().press(Pi.PRESS_START_PIN);
+Pi.getInstance().watch(Pi.RED_VICTORY_PIN);
